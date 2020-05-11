351df1929690488ce3b4e072ede51c8aa826eda8
blodemaster <blodemaster@gmail.com>
2020-05-11 20:19:23 +0200

    update deployment files

--- Microservice/udagram-api-feeditem/src/aws.ts
+++ Microservice/udagram-api-feeditem/src/aws.ts
@@ -3,7 +3,11 @@ import {config} from './config/config';
 
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+// const credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+if(config.aws_profile !== "DEPLOYED") {
+  var credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
+  AWS.config.credentials = credentials;
+}
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
