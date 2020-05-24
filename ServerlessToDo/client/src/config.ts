60480be94b9d337c58cb1855db712a6eed4f48e3
blodemaster <blodemaster@gmail.com>
2020-05-24 11:17:03 +0200

    add tracing

--- ServerlessToDo/client/src/config.ts
+++ ServerlessToDo/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'kvutwvr9hd'
+const apiId = 'k2e2rmn402'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-north-1.amazonaws.com/dev`
 
 export const authConfig = {
